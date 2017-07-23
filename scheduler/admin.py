526cf1f50ca6ee13ad2cf11dd7a48b47ebc289ad
Stephanie Moyerman <smoyerman@gmail.com>
2017-07-23 02:08:51 +0000

    Add Days model to admin interface

--- scheduler/admin.py
+++ scheduler/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Resident, Service
+from .models import Resident, Service, Day
 
 class ResidentAdmin(admin.ModelAdmin):
     list_display = ('lname','fname','year')
@@ -7,6 +7,10 @@ class ResidentAdmin(admin.ModelAdmin):
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('month','onservice','res')
 
+class DayAdmin(admin.ModelAdmin):
+    list_display = ('date','residents')
+
 # Register your models here.
 admin.site.register(Resident, ResidentAdmin)
 admin.site.register(Service, ServiceAdmin)
+admin.site.register(Day, DayAdmin)
